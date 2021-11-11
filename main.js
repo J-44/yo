cf46dacc5da24a96d2aa51afd2d020f960021889
J-44 <74297043+J-44@users.noreply.github.com>
2021-11-11 10:14:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    var synth = window.speechSynthesis;
+    speak_data = " Welcome to our amazing adventure";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+function dandy(){
+    var ynth = window.speechSynthesis;
+    peak_data = " Location of tropical evergreen forest ,  So as we know Tropical Evergreen Forest is found in places which receive more than 200cm rainfall. I am going to give you a look that where are these foresets found and a small video of one of the Tropical Evergreen Forest";
+    var uterThis = new SpeechSynthesisUtterance(peak_data);
+    ynth.speak(uterThis);
+}
+function dady(){
+    var snth = window.speechSynthesis;
+    eak_data = " So as you can see most of the Tropical evegreen forest is found on south america. The places in India having tropical evergreen forest are   Tamil Nadu , Kerala , Karnataka , Maharashtra , Assam , Arunachal Pradesh , Nagaland , Tripura , Meghalaya , West Bengal , Andaman and Nicobar Islands";
+    var uerThis = new SpeechSynthesisUtterance(eak_data);
+    snth.speak(uerThis);
+}
+function ady(){
+    var nth = window.speechSynthesis;
+    ak_data = " Climate in tropical evergreen forest , Climate in tropical evergreen forest is wet and humid because of heavy rainfall , the average annual temperature is between 25°C and 27°C and average annual humidity exceeds 77 percent in tropical evegreen forest";
+    var uerhis = new SpeechSynthesisUtterance(ak_data);
+    nth.speak(uerhis);
+}
+function dy(){
+    var th = window.speechSynthesis;
+    k_data = " Wildlife in tropical evergreen forest . Here a lot of variety of animals found in tropical evergreen like Elephants , Monkeys , Lemur , Deer , One-Horned Rhino (Assam, West Bengal) , Birds , Bats , Sloth , Scorpions , Snails Et cetera";
+    var uerhi = new SpeechSynthesisUtterance(k_data);
+    th.speak(uerhi);
+} 
+function y(){
+    var h = window.speechSynthesis;
+    _data = " Some fun facts about tropical evergreen forest are . Tropical evergreen forest almost cover 7% of the earth’s surface. They are mostly found near the equator. It is also supports our ecosystem. These forest are multi-layered and dense.";
+    var ueri = new SpeechSynthesisUtterance(_data);
+    h.speak(ueri);
+}
+function day(){
+    var hat = window.speechSynthesis;
+    _ata = " Vegetation in tropical evergreen forest , Since this region receives a lot of amount of rainfall , that is why here all variety of plants are avilable trees, shrubs, and creepers. Some of the plants are Ebony , Mahogany , Rosewood , Rubber Chinchona Et cetera.";
+    var j = new SpeechSynthesisUtterance(_ata);
+    hat.speak(j);
+}
